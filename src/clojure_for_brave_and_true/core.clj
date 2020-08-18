c1bb204f23b29d7e4e3ab1f3d9ef653be196aaea
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-08-18 09:48:37 -0700

    Chap 1, Ex 1, added hash-map function

--- src/clojure_for_brave_and_true/core.clj
+++ src/clojure_for_brave_and_true/core.clj
@@ -9,4 +9,5 @@
   (println "Ch. 1 Ex.1 str: " (str "This is was not a string -> " 1))
   (println "Ch. 1 Ex.1 vector: " ( vector  "This" "was" "not" "a" "vector"))
   (println "Ch. 1 Ex.1 list: " ( list  "This" "was" "not" "a" "list"))
+  (println "Ch. 1 Ex.1 hashmap: " ( hash-map :goodfood "ravioli" :badfood "lettuce" ))
  )
