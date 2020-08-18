81157ac247ed7e9fc4522274ce25fdadc347b494
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-08-18 09:43:40 -0700

    Added Chap 1, Ex 1, List

--- src/clojure_for_brave_and_true/core.clj
+++ src/clojure_for_brave_and_true/core.clj
@@ -2,6 +2,11 @@
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Exercises from Clojure for the Brave and True"
   [& args]
-  (println "Hello, World!"))
+  ; Chapter 1, ex. 1: Use the str, vector, list, hash-map, and hash-set functions.
+
+  (println "Ch. 1 Ex.1 str: " (str "This is was not a string -> " 1))
+  (println "Ch. 1 Ex.1 vector: " ( vector  "This" "was" "not" "a" "vector"))
+  (println "Ch. 1 Ex.1 list: " ( list  "This" "was" "not" "a" "list"))
+ )
