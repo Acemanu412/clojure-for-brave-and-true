82a14a294850cca42186dfeacfcdb8befc1ed904
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-08-18 09:51:31 -0700

    Ch.1 Ex 1, added hash-set function

--- src/clojure_for_brave_and_true/core.clj
+++ src/clojure_for_brave_and_true/core.clj
@@ -9,5 +9,6 @@
   (println "Ch. 1 Ex.1 str: " (str "This is was not a string -> " 1))
   (println "Ch. 1 Ex.1 vector: " ( vector  "This" "was" "not" "a" "vector"))
   (println "Ch. 1 Ex.1 list: " ( list  "This" "was" "not" "a" "list"))
-  (println "Ch. 1 Ex.1 hashmap: " ( hash-map :goodfood "ravioli" :badfood "lettuce" ))
+  (println "Ch. 1 Ex.1 hash-map: " ( hash-map :goodfood "ravioli" :badfood "lettuce" ))
+  (println "Ch. 1 Ex.1 hash-set: " ( hash-set 1 2 3 3 3 3 3 3 3 4 5 ))
  )
