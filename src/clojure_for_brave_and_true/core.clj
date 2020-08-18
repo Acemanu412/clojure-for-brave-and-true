f9a4241c5bc495ec982effb81175eb1025a0998e
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-08-18 10:19:47 -0700

    Ch.1 Ex.1 dec-maker

--- src/clojure_for_brave_and_true/core.clj
+++ src/clojure_for_brave_and_true/core.clj
@@ -11,10 +11,19 @@
     [x]
     (+ x 100))
 
+  (defn dec-maker
+    "Create a custom decrementor"
+    [dec-by]
+    #(- % dec-by))
+
+  (def dec3 (dec-maker 3))
+  (def dec9 (dec-maker 9))
   (println "Ch. 1 Ex.1 str: " (str "This is was not a string -> " 1))
   (println "Ch. 1 Ex.1 vector: " ( vector  "This" "was" "not" "a" "vector"))
   (println "Ch. 1 Ex.1 list: " ( list  "This" "was" "not" "a" "list"))
   (println "Ch. 1 Ex.1 hash-map: " ( hash-map :goodfood "ravioli" :badfood "lettuce" ))
   (println "Ch. 1 Ex.1 hash-set: " ( hash-set 1 2 3 3 3 3 3 3 3 4 5 ))
   (println "Ch. 1 Ex.1 add-100: " ( add-100 50 ))
+  (println "Ch. 1 Ex.1 dec-maker 3: " ( dec3 10 ))
+  (println "Ch. 1 Ex.1 dec-maker 9: " ( dec9 10 ))
  )
